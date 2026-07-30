2b643ed090616c4ac1b0842d995c69e40593f626
LurissMods <neednameforusername@gmail.com>
2026-07-31 09:40:23 +1000

    Updated mod.cpp version to 0.2.4.0

--- mod.cpp
+++ mod.cpp
@@ -1,6 +1,6 @@
 author = "Luriss";
 name = "Extraterra";
-description = "Extraterra - Version 0.2.3.0";
+description = "Extraterra - Version 0.2.4.0";
 overview = "";
 overviewPicture = "logo_exterra_ca.paa";
 picture = "logo_exterra.png";
